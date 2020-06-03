eece9d460d2add8f9ed5040655e1d10d07d069fe
Tomasz Lewicki <t.w.lewicki@gmail.com>
2020-06-02 21:40:10 -0700

    updated inference demo

--- demo_local/demo_jetson.py
+++ demo_local/demo_jetson.py
@@ -1,15 +1,36 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+
 import requests
 import matplotlib.pyplot as plt
 
 import time
 import os
 
+
 import torch
 from torch import nn
 from torchvision.models import resnet50
 import torchvision.transforms as T
 torch.set_grad_enabled(False)
+
+# COCO classes
+CLASSES = [
+    'N/A', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
+    'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+    'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack',
+    'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+    'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+    'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass',
+    'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+    'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
+    'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A',
+    'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+    'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A',
+    'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+    'toothbrush'
+]
+
 
 class DETRdemo(nn.Module):
     """
@@ -81,7 +102,6 @@ class DETRdemo(nn.Module):
         return {'pred_logits': self.linear_class(h), 
                 'pred_boxes': self.linear_bbox(h).sigmoid()}
 
-
 # for output bounding box post-processing
 def box_cxcywh_to_xyxy(x):
     x_c, y_c, w, h = x.unbind(1)
@@ -120,25 +140,30 @@ def plot_results(pil_img, prob, boxes):
                                    fill=False, color=c, linewidth=3))
         cl = p.argmax()
         text = f'{CLASSES[cl]}: {p[cl]:0.2f}'
-        ax.text(xmin, ymin, text, fontsize=15,
-                bbox=dict(facecolor='yellow', alpha=0.5))
+        ax.text(xmin, ymin, text, fontsize=8,
+                bbox=dict(facecolor='white', alpha=0.3))
     plt.axis('off')
     plt.show()
 
-def save_image(pil_img, prob, boxes, filename):
-    plt.figure(figsize=(16,10))
-    plt.imshow(pil_img)
-    ax = plt.gca()
-    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), COLORS * 100):
-        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
-                                   fill=False, color=c, linewidth=3))
+COLORS = ['red', 'green', 'blue', 'pink', 'yellow']
+
+def overlay_bboxes(pil_img, prob, boxes):
+    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), COLORS * 100):        
         cl = p.argmax()
         text = f'{CLASSES[cl]}: {p[cl]:0.2f}'
-        ax.text(xmin, ymin, text, fontsize=15,
-                bbox=dict(facecolor='yellow', alpha=0.5))
-    plt.axis('off')
-    plt.savefig(filename)
-
+        
+        draw = ImageDraw.Draw(pil_img)
+        
+        # draw bounding box
+        shape = [(xmin, ymin), (xmax, ymax)] 
+        draw.rectangle(shape, fill=None, outline=c, width=2)
+        
+        # draw text box
+        font = ImageFont.truetype("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf", 18, encoding="unic")
+        draw.text((xmin, ymin), text, font=font)
+        
+    return pil_img
+        
 if __name__ == '__main__':
 
     detr = DETRdemo(num_classes=91)
@@ -150,46 +175,24 @@ if __name__ == '__main__':
     detr.eval()
     detr = detr.to('cuda')
 
-    # COCO classes
-    CLASSES = [
-        'N/A', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-        'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
-        'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
-        'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack',
-        'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
-        'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
-        'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass',
-        'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
-        'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
-        'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A',
-        'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
-        'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A',
-        'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
-        'toothbrush'
-    ]
-
-    # colors for visualization
-    COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
-            [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
-
     # standard PyTorch mean-std input image normalization
     transform = T.Compose([
-        T.Resize(800),
+        T.Resize((800,600)),
         T.ToTensor(),
         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-
-    in_dir = '/home/tomek/projects/detr/demo_local/frames/'
-    out_dir = '/home/tomek/projects/detr/demo_local/frames_out/'
+    in_dir = '/home/tomek/projects/detr/demo_local/frames50fps/'
+    out_dir = '/home/tomek/projects/detr/demo_local/frames50fpsout/'
 
     fnames = list(os.listdir(in_dir))
 
     for fname in fnames:
-        im = Image.open(in_dir+fname)
         st = time.time()
+        im = Image.open(in_dir+fname)
         scores, boxes = detect(im, detr, transform)
+        im = overlay_bboxes(im,scores,boxes)
+        im.save(out_dir+fname)
         print(time.time()-st)
-        save_image(im, scores, boxes, out_dir+fname)
         # plot_results(im, scores, boxes)
 
